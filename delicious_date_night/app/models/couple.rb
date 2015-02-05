bafd113c565bab9c3fc833c7f9f68598a14dc31d
Tovarm <tovamosk@gmail.com>
2015-02-05 14:54:25 -0500

    changed favorite and datenight models

--- delicious_date_night/app/models/couple.rb
+++ delicious_date_night/app/models/couple.rb
@@ -1,5 +1,5 @@
 class Couple < ActiveRecord::Base
-	has_many :dates,
-	has_many :favorites,
-	has_many :events, {through: :favorites}
+	self.has_many :dates
+	self.has_many :favorites
+	self.has_many :events, {through: :favorites}
 end
