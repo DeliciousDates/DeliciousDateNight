9e82ea96a746e1a0a858fd92a4e3664bcf643e46
alexwang949 <alex_cellist@yahoo.com>
2015-02-06 16:17:09 -0500

    events/new events:id

--- delicious_date_night/app/controllers/events_controller.rb
+++ delicious_date_night/app/controllers/events_controller.rb
@@ -1,16 +1,21 @@
 class EventsController < ApplicationController
 
 	def new
+		@foods = Food.all
+		@drinks = Drink.all
+		@activities = Activity.all
+		@id = params[:couple_id]
 
 	end
 
 	def create
-		@id = params[:couple_id]
+		Event.create({food_id: params[:food], drink_id: params[:drink], event_theme: params[:theme], couple_id: params[:couple_id]})
+		redirect_to("/couples/" + params[:couple_id] + "/events")
 	end
 
 	def show
 		@event = Event.find(params[:id])
-		# //find the event by the event id, no need for couple id.
+
 	end
 
 	def edit
