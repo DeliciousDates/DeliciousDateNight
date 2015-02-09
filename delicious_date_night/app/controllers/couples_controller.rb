0f833557ee4eed646f7b828297b89cc547d1a08c
Tovarm <tovamosk@gmail.com>
2015-02-09 16:54:15 -0500

    added to about page and changed forms to require fields

--- delicious_date_night/app/controllers/couples_controller.rb
+++ delicious_date_night/app/controllers/couples_controller.rb
@@ -42,9 +42,11 @@ class CouplesController < ApplicationController
 	# add new couple instance to the database
 	def create
 		# binding.pry
+		if 
 		@couple = Couple.create({a_fname: params["a_fname"], a_lname: params["a_lname"], a_age: params["a_age"], a_gender: params["a_gender"], a_email: params["a_email"], b_fname: params["b_fname"], b_lname: params["b_lname"], b_age: params["b_age"], b_gender: params["b_gender"], b_email: params["b_email"], picture_url: params["picture_url"], description: params["description"], password: params[:password], avatar: params[:avatar]})
 		# binding.pry
 		redirect_to "/"
+	end
 	end
 
 	# GET /couples/:id/profile
