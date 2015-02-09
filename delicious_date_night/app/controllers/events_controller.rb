6f233ee009b7704341ee7e4df1a725cb7edc4be9
alexwang949 <alex_cellist@yahoo.com>
2015-02-09 10:12:11 -0500

    put events

--- delicious_date_night/app/controllers/events_controller.rb
+++ delicious_date_night/app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
 
 	def create
 		Event.create({food_id: params[:food], drink_id: params[:drink], activity_id: params[:activity], event_theme: params[:theme], couple_id: params[:couple_id]})
-		redirect_to("/couples/" + params[:couple_id] + "/events")
+		redirect_to("/couples/" + params[:couple_id])
 	end
 
 	def show
@@ -18,11 +18,17 @@ class EventsController < ApplicationController
 	end
 
 	def edit
-
+		@event = Event.find(params[:id])
+		@foods = Food.all
+		@drinks = Drink.all
+		@activities = Activity.all
+		@id = params[:couple_id]
 	end
 
 	def update
-
+		@event = Event.find_by(id: params[:id])
+		updated = @event.update({food_id: params[:food], drink_id: params[:drink], activity_id: params[:activity], event_theme: params[:theme], couple_id: params[:couple_id]})
+		redirect_to("/couples/" + params[:couple_id])
 	end
 
 	def destroy
