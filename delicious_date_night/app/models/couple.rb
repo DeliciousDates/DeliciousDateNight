f104896e64dba0bd5d150725aade7fc5e1b13531
gretchenziegler <gretchenziegler@gmail.com>
2015-02-09 09:23:15 -0500

    added authorize function to couple model; changed picture in date night form to reflect activitiy; redirected edit couple to couple profile

--- delicious_date_night/app/models/couple.rb
+++ delicious_date_night/app/models/couple.rb
@@ -6,4 +6,14 @@ class Couple < ActiveRecord::Base
 	self.has_many :dates
 	self.has_many :favorites
 	self.has_many :events, {through: :favorites}
+
+	# authorize a user based on his or her session id
+	def authorize(couple)
+    unless couple.id == session[:couple_id]
+      render(text: 'Unauthorized', status: 401) and return true
+    end
+
+    return false
+  end
+
 end
