32e723b398c9790e275666ef92f909470b8503b6
Tovarm <tovamosk@gmail.com>
2015-02-25 14:29:49 -0500

    fixed couples profile page

--- delicious_date_night/app/controllers/couples_controller.rb
+++ delicious_date_night/app/controllers/couples_controller.rb
@@ -20,7 +20,7 @@ class CouplesController < ApplicationController
 	def update
 		@id = (params[:id])
 		@couple = Couple.find(params["id"])
-		@couple.update(a_fname: params["a_fname"], a_lname: params["a_lname"], a_age: params["a_age"], a_gender: params["a_gender"], a_email: params["a_email"], b_fname: params["b_fname"], b_lname: params["b_lname"], b_age: params["b_age"], b_gender: params["b_gender"], b_email: params["b_email"], picture_url: params["picture_url"], description: params["description"], avatar: params[:avatar])
+		@couple.update(a_fname: params["a_fname"], a_lname: params["a_lname"], a_age: params["a_age"], a_gender: params["a_gender"], a_email: params["a_email"], b_fname: params["b_fname"], b_lname: params["b_lname"], b_age: params["b_age"], b_gender: params["b_gender"], b_email: params["b_email"], picture_url: params["picture_url"], description: params["description"], avatar: params[:avatar], city: params[:city])
 
 		redirect_to "/profile"
 	end
@@ -42,7 +42,7 @@ class CouplesController < ApplicationController
 	# POST /couples
 	# add new couple instance to the database
 	def create
-		@couple = Couple.create({a_fname: params["a_fname"], a_lname: params["a_lname"], a_age: params["a_age"], a_gender: params["a_gender"], a_email: params["a_email"], b_fname: params["b_fname"], b_lname: params["b_lname"], b_age: params["b_age"], b_gender: params["b_gender"], b_email: params["b_email"], description: params["description"], password: params[:password], username: params[:username], avatar: params[:avatar]})
+		@couple = Couple.create({a_fname: params["a_fname"], a_lname: params["a_lname"], a_age: params["a_age"], a_gender: params["a_gender"], a_email: params["a_email"], b_fname: params["b_fname"], b_lname: params["b_lname"], b_age: params["b_age"], b_gender: params["b_gender"], b_email: params["b_email"], description: params["description"], password: params[:password], username: params[:username], avatar: params[:avatar], city: params[:city]})
 
 		redirect_to "/session/new"
 	end
